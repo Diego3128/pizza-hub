00edcc6c281cd174a123dd7fe90fd225566f360c
Diego3128 <145170018+Diego3128@users.noreply.github.com>
2025-11-25 12:32:43 -0500

    feat: location section with google maps iframe

--- app/components/Location.tsx
+++ app/components/Location.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { Title } from "./elements/Title";
+
 export const Location = () => {
   return (
-    <div>Location</div>
-  )
-}
+    <div>
+      <Title level={2}>Where to find us</Title>
+      <div className="w-11/12 xl:w-full mx-auto rounded-xl overflow-hidden">
+        <iframe
+          width="100%"
+          height="600"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=7.117486569975103,%20-73.11132878293054+(Pizzahub)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+        </iframe>
+      </div>
+    </div>
+  );
+};
